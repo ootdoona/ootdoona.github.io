8a046547394113499934f91c97ab330a74334402
Eunah Jung <oonatunakuna@gmail.com>
2021-03-07 20:16:24 +0100

    Updates

---
+++ precache-manifest.8adfb61b3eeb5b274fdf830d886e3576.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc99a4979c4e211ed716f007f363cf77",
+    "url": "/ReactFolio/index.html"
+  },
+  {
+    "revision": "3fde2b61685d689eb3d5",
+    "url": "/ReactFolio/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/ReactFolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ReactFolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fde2b61685d689eb3d5",
+    "url": "/ReactFolio/static/js/main.692e4464.chunk.js"
+  },
+  {
+    "revision": "cc897b93664c64c1287e",
+    "url": "/ReactFolio/static/js/runtime-main.20217e57.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/ReactFolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/ReactFolio/static/media/your photo.b9d7d448.png"
+  }
+]);
