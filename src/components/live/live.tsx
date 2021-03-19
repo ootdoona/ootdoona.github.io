93154389b6e65dcf858f75be73fac554117a0bd9
Eunah Jung <oonatunakuna@gmail.com>
2021-03-19 20:32:41 +0100

    update gif assets & update ko text draft

--- src/components/live/live.tsx
+++ src/components/live/live.tsx
@@ -15,7 +15,7 @@ function Live(props: LiveProps) {
 					{content.title}
 				</div>
 				<div className="livestream">
-    			   <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F100001820358579%2Fvideos%2F5294837047253588%2F&width=500&show_text=false&appId=1249604281777304&height=889" width="100%" height="500" scrolling="no" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+				<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F100001761559477%2Fvideos%2F3747788025289879%2F&width=500&show_text=false&appId=1249604281777304&height=281" width="500" height="281" scrolling="no" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" ></iframe>
 				</div>
 				<div className="line">-</div>
 				<div className="para">
