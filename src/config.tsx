8f398837d230e735c4b24c42997217124e81ab38
Eunah Jung <oonatunakuna@gmail.com>
2021-03-20 23:49:03 +0100

    set timezone kst

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,8 @@
 import moment from 'moment';
 
 export const timeline = [
-    '3/20/2021/23:23:00',
-    '3/20/2021/23:30:00'
+    '3/21/2021/20:45:00+09:00',
+    '3/21/2021/22:45:00+09:00'
 ]
 export const fblink: string[] = [
     "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fwoojaega%2Fvideos%2F913576439405391%2F&width=1280",
