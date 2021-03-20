c66fe06cec14317fdb0f598f83a6262bbea369bd
Eunah Jung <oonatunakuna@gmail.com>
2021-03-20 23:28:56 +0100

    update text

--- src/contents.tsx
+++ src/contents.tsx
@@ -15,13 +15,13 @@ export const language = {
 
 export const introContent = {
     title: "Eros, as a Modern Punishment",
-    para1: "이 사회에서 관계는 억압이다. 자신에게 확실하고 이익이 되는 것만을 쫓는 개인의 습성이 타인의 세계, 그 무한성을 소멸시킨다. 관계에는 개개인 머릿속의 체계만이 남는다. 에로스를 잠식한다. 에로스는 타인 그 자체를 욕망하는 마음이며, 타인의 세계로 인한 변화나 희생 — 이는 절대적 손실과 다르다 — 은 삶에 대한 열정이다. 이를 따르려면 이 사회가 말하는 “선”에 반(反)해야 할 지경이다. 그렇게 만들어진 작품들은 여느 타인의 몸에 의해 변화하여 아름다운 우리 몸을 보인다. 이러한 현상은 누군가에게는 그 자체로서 벌이다.",
-    para2: "퍼포먼스/전시는 지역 사회의 예술 공간과 웹에서 발생한다. 민주적 접근성을 위해 지역을 위한 공간과 협력하고, 현대미술과 일상적 삶 사이의 새로운 관계를 제시한다. 동시에, 판데믹에 처한 오늘날, 퍼포먼스 영상은 대체재가 아닌 통합적(holistic) 퍼포먼스의 일부로서 SNS에 생중계 되며, 지역과 글로벌의 역할을 달리 보고 새롭게 연결한다. 이러한 시도는 작가가 추구해 온 퍼포먼스-이미지간 초학문적 상호성에 기반하고 있다."
+    para1: "이 사회에서 관계는 억압이다. 자신에게 확실하고 이익이 되는 것만을 쫓는 개인의 습성이 타인의 세계, 그 무한성을 소멸시킨다. 관계에는 개개인 머릿속의 체계만이 남는다. 에로스를 잠식한다. 에로스는 타인 그 자체를 욕망하는 마음이며, 타인의 세계로 인한 변화나 희생은 삶에 대한 열정이다. 이 욕망을 따르려면 이 사회가 말하는 “선”에 반(反)해야 할 지경이다. 그렇게 만들어진 작품들은 여느 타인의 몸에 의해 변화하여 아름다운 우리 몸을 보인다. 이러한 현상은 누군가에게는 그 자체로서 벌이다.",
+    para2: "퍼포먼스/전시는 지역 사회의 예술 공간과 웹에서 발생한다. 민주적 접근성을 위해 지역을 위한 공간과 협력하며 현대미술과 일상적 삶 사이의 새로운 관계를 제시한다. 동시에 판데믹에 처한 오늘날, 퍼포먼스 영상은 대체재가 아닌 통합적(holistic) 퍼포먼스의 일부로서 SNS에 생중계 되며 지역과 글로벌의 역할을 달리 보고 새롭게 연결한다. 이러한 시도는 작가가 추구해 온 퍼포먼스-이미지간 초학문적 상호성에 기반하고 있다."
 }
 export const introContentEn = {
     title: "Eros, as a modern punishment",
-    para1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    para2: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    para1: "In South Korean society, freedom is suppressed by relationships. Individuals fetishize their own certainties and advantages, ignorant of other people as infinity. In a relationship only an individual’s system or calculation matters. Eros is destroyed. Eros is pure desire towards the other, and is a passion for life which allows us to suffer the changes and sacrifices caused by the other. To follow this desire, it is necessary to be opposed to the “virtue” wherein the society has been rooted. As such, the works which manifest our bodies being changed by other people’s bodies appear to be beautiful. Yet this phenomenon itself is a punishment for someone.",
+    para2: "Performance/Exhibition is run through local art spaces and a website. For democratic accessibility, it cooperates with local spaces, proposing a new relation between contemporary art and everyday life. Meanwhile, during today’s pandemic, performance video, not as a replacement but as a part of holistic performance, is live-streamed on SNS. Here the different roles of the local and the global are newly connected. This trial is based on the artist’s pursuit of a transdisciplinary performance-image."
 }
 
 export const personalInfo = {
@@ -33,12 +33,12 @@ export const personalInfo = {
 }
 
 export const liveContent = {
-    title: "ACT I. 거울 앞에서",
-    para1: "개인이 인식하는 자신의 정체성 속에는 맹점이 있다. 그가 보는 다른 사람에는 자신이 반영되어 있지만, 종종 자신은 그사람과 다르다고 구분 짓는다. 거울 앞에서, 퍼포머는 관객들 사이에 형성된 사회적 위계를 노골적인 얼굴과 제스처로 드러낸다.",
+    title: "ACT I. 거울앞에서",
+    para1: "개인이 인식하는 자신의 정체성 속에는 맹점이 있다. 그가 보는 다른 사람에는 자신이 누구인지와 어떻게 그 사람으로부터 자신을 구분 짓는지 반영되어 있다. 거울 앞에서, 퍼포머는 관객들이 형성한 사회적 위계, 질서를 노골적인 얼굴과 제스처로 드러낸다."
 }
 export const liveContentEn = {
-    title: "ACT I. asdfasdf",
-    para1: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    title: "ACT I. In Front of the Mirror",
+    para1: "Individuals often have blindness towards their own identities. What they see in other people is reflected by who they are and how they differentiate themselves from others. In front of the mirror, a performer uses their face and gestures to reveal the social hierarchy and order that audience members make."
 }
 
 export const teamInfo = {
