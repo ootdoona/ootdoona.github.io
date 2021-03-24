c861b9c63aa9d5e988315b6e0e9c9aace4db7f36
Eunah Jung <oonatunakuna@gmail.com>
2021-03-25 00:01:17 +0100

    update text

--- src/contents.tsx
+++ src/contents.tsx
@@ -16,12 +16,12 @@ export const language = {
 export const introContent = {
     title: "Eros, as a Modern Punishment",
     para1: "이 사회에서 관계는 억압이다. 자신에게 확실하고 이익이 되는 것만을 쫓는 개인의 습성이 타인의 세계, 그 무한성을 소멸시킨다. 관계에는 개개인 머릿속의 체계만이 남는다. 에로스를 잠식한다. 에로스는 타인 그 자체를 욕망하는 마음이며, 타인의 세계로 인한 변화나 희생은 삶에 대한 열정이다. 이 욕망을 따르려면 이 사회가 말하는 “선”에 반(反)해야 할 지경이다. 그렇게 만들어진 작품들은 여느 타인의 몸에 의해 변화하여 아름다운 우리 몸을 보인다. 이러한 현상은 누군가에게는 그 자체로서 벌이다.",
-    para2: "퍼포먼스/전시는 지역 사회의 예술 공간과 웹에서 발생한다. 민주적 접근성을 위해 지역을 위한 공간과 협력하며 현대미술과 일상적 삶 사이의 새로운 관계를 제시한다. 동시에 판데믹에 처한 오늘날, 퍼포먼스 영상은 대체재가 아닌 통합적(holistic) 퍼포먼스의 일부로서 SNS에 생중계 되며 지역과 글로벌의 역할을 달리 보고 새롭게 연결한다. 이러한 시도는 작가가 추구해 온 퍼포먼스-이미지간 초학문적 상호성에 기반하고 있다."
+    para2: "퍼포먼스/전시는 지역의 예술 공간과 웹에서 발생한다. 지역 관객들과 밀접하게 소통한다. 동시에 판데믹에 처한 오늘날, 퍼포먼스 영상은 대체재가 아닌 통합적(holistic) 퍼포먼스의 일부로서 SNS에 생중계 되며 지역과 글로벌의 역할을 달리 보고 새롭게 연결한다. 이러한 시도는 작가가 추구해 온 퍼포먼스-이미지간 초학문적 상호성에 기반하고 있다."
 }
 export const introContentEn = {
     title: "Eros, as a Modern Punishment",
     para1: "In South Korean society, freedom is suppressed by relationships. Individuals fetishize their own certainties and advantages, ignorant of other people as infinity. In a relationship only an individual’s system or calculation matters. Eros is destroyed. Eros is pure desire towards the other, and is a passion for life which allows us to suffer the changes and sacrifices caused by the other. To follow this desire, it is necessary to be opposed to the “virtue” wherein the society has been rooted. As such, the works which manifest our bodies being changed by other people’s bodies appear to be beautiful. Yet this phenomenon itself is a punishment for someone.",
-    para2: "Performance/Exhibition is run through local art spaces and a website. For democratic accessibility, it cooperates with local spaces, proposing a new relation between contemporary art and everyday life. Meanwhile, during today’s pandemic, performance video, not as a replacement but as a part of holistic performance, is live-streamed on SNS. Here the different roles of the local and the global are newly connected. This trial is based on the artist’s pursuit of a transdisciplinary performance-image."
+    para2: "Performance/Exhibition is run through local art spaces and a website. It focuses on intimate communication with local audiences. Meanwhile, during today’s pandemic, performance video, not as a replacement but as a part of holistic performance, is live-streamed on SNS. The different roles of the local and the global are newly connected. This trial is based on the artist’s pursuit of a transdisciplinary performance-image."
 }
 
 export const personalInfo = {
